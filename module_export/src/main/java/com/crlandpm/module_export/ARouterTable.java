ee07c9d219423b1936bd0535292e6b3782baed28
杨光 <yangguang112@crland.com.cn>
2022-03-02 11:05:47 +0800

    解决an annotation argument must be a compile-time constant

---
+++ module_export/src/main/java/com/crlandpm/module_export/ARouterTable.java
@@ -0,0 +1,12 @@
+package com.crlandpm.module_export;
+
+/**
+ * Author: Danny Yang
+ * Date: 2022/03/02/10:42 上午
+ */
+
+public interface ARouterTable {
+    String PATH_A = "/componenta/moduleaActivity";
+    String PATH_SERVICE_A = "/componenta/service";
+    String PAth_FRAGMENT_A = "/componenta/ModuleFragment";
+}
